a8eb63bfc5ae634cba239a50606e5884e9ae08ed
keepon-01 <971171923@qq.com>
2024-03-09 22:55:39 +0800

    add receive packets fuction and add thread example

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include "sys_plat.h"
 
-int main(void)
+void thread1_entry(void * arg)
 {
+    while (1)
+    {
+        plat_printf("this is thread1 %s\n", (char *)arg);
+        sys_sleep(100);
+    }
+}
+
+void thread2_entry(void * arg)
+{
+    while (1)
+    {
+        plat_printf("this is thread2 %s\n", (char *)arg);
+        sys_sleep(100);
+    }
+}
+int main(void)
+{   
+    //thread example
+    sys_thread_create(thread1_entry, "aaaa");
+    sys_thread_create(thread2_entry, "bbbb");
+
+
     pcap_t * pcap = pcap_device_open(netdev0_phy_ip, netdev0_hwaddr);
     while(pcap)
     {
@@ -12,6 +34,20 @@ int main(void)
         {
             buffer[i] = i;
         }
+
+        //receive packets
+        struct pcap_pkthdr  *pkthdr;
+        const uint8_t * pkt_data;
+        if(pcap_next_ex(pcap, &pkthdr, &pkt_data) != 1)
+        {
+            continue;
+        }
+
+        int len = pkthdr->len > sizeof(buffer) ? sizeof(buffer) : pkthdr->len;
+        plat_memcpy(buffer, pkt_data, len);
+        buffer[0] = 1;
+        buffer[1] = 2;
+
 
         if(pcap_inject(pcap, buffer, sizeof(buffer)) == -1)
         {
